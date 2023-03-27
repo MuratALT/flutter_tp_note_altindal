7ecec53f07ce58258eac2feee4178107b2d900c2
altindal1u <murat.altindal5@etu.univ-lorraine.fr>
2023-03-27 14:11:04 +0200

    Finalisation de l'appel des salles à l'API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,35 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter EDT iutInfo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontWeight: FontWeight.bold),
+          bodyMedium: TextStyle(fontSize: 18.0, fontFamily: 'Hind'),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter EDT iutInfo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,17 +37,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<dynamic> _rooms = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    _fetchRooms();
+
+    super.initState();
+  }
+
+  Future<List<dynamic>> _fetchRooms() async {
+    final url = Uri.parse(
+        "https://dptinfo.iutmetz.univ-lorraine.fr/applis/flutter_api_s1/api/rooms/findAllMachine.php");
+
+    final response = await http.get(Uri.parse(
+        "https://dptinfo.iutmetz.univ-lorraine.fr/applis/flutter_api_s1/api/rooms/findAllMachine.php"));
+
+    if (response.statusCode == 200) {
+      print(json.decode(response.body));
+      return json.decode(response.body);
+    } else {
+      throw Exception('Les données n\'ont pas pu être chargées correctement.');
+    }
+  }
+
+  Future<Widget> _buildRoomsCheckbox() async {
+    var listOfRooms = await _fetchRooms();
+
+    return ListView.builder(
+      itemCount: listOfRooms.length,
+      itemBuilder: (context, index) {
+        final room = listOfRooms[index];
+        return Row(
+          children: [
+            Text(room['nom']),
+            Checkbox(
+              value: false,
+              onChanged: (newValue) {},
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -99,14 +119,14 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '0',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _fetchRooms,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
